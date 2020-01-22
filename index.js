e9c86c47cfdf035a2d5054291b3033f7066963ab
Maksym Kondratenko <maksym.kondratenko@gmail.com>
2020-01-22 10:13:48 +0000

    performance

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const nemo = ['a', 'b'];
-console.log(nemo);
+const nemo = ['a', 'nemo'];
+const nemo2 = ['a', 'd', 'dd', 'dd', 'dd', 'dd', 'nemo'];
+const large = new Array(100000).fill('nemo');
+
+function findNemo(arr) {
+  let t0 = performance.now();
+  for(i = 0; i < arr.length; i++) {
+    if (arr[i] === 'nemo') {
+     // console.log('exists');
+    }
+  }
+  let t1 = performance.now();
+  console.log('time to execute is ', (t1-t0))
+}
+
+findNemo(large)
